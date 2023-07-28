51af1d486460db4346cd32daee7c74749d9fb7d5
AndriiSmolinskyi <awful.braid@gmail.com>
2023-07-28 14:28:43 +0300

    1

--- App.js
+++ App.js
@@ -1,11 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, FlatList, StyleSheet, RefreshControl } from 'react-native';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 export default function App() {
+  const [items, setItems] = useState([]);
+  const [refreshing, setRefreshing] = useState(false);
+
+  const fetchData = () => {
+    axios('https://jsonplaceholder.typicode.com/posts')
+      .then(response => setItems(response.data))
+      .finally(() => setRefreshing(false));
+  };
+
+  useEffect(() =>{
+    fetchData();
+  }, []);
+
+  const onRefresh = () => {
+    setRefreshing(true);
+    fetchData();
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <FlatList
+        data={items}
+        renderItem={({ item }) => (
+          <View key={item.id}>
+            <Text>{item.title}</Text>
+            <Text>{item.body}</Text>
+            <Text></Text>
+          </View>
+        )}
+        keyExtractor={(item) => item.id.toString()}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
+      />
     </View>
   );
 }
