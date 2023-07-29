b3bb1c050cfcbf0093a78b17622d707a5ae6900e
AndriiSmolinskyi <awful.braid@gmail.com>
2023-07-29 12:47:46 +0300

    1

--- App.js
+++ App.js
@@ -1,52 +1,52 @@
-import { View, Text, FlatList, StyleSheet, RefreshControl } from 'react-native';
-import { useState, useEffect } from 'react';
+// http://31.43.107.151:7321/api/
+
+
+import React, { useState } from 'react';
+import { View, TextInput, Button, Alert, StatusBar } from 'react-native';
 import axios from 'axios';
 
+const BASE_URL = 'http://31.43.107.151:7321/api';
+
 export default function App() {
-  const [items, setItems] = useState([]);
-  const [refreshing, setRefreshing] = useState(false);
+  const [phone, setPhone] = useState('');
 
-  const fetchData = () => {
-    axios('https://api.evos.in.ua/tariffs')
-      .then(response => setItems(response.data))
-      .catch((error) => console.error('pomilka'))
-      .finally(() => setRefreshing(false));
-  };
+  const handleSendSms = async () => {
+    try {
+      const response = await axios.post(`${BASE_URL}/account/register/sendConfirmCode`, {
+        phone: phone,
+      });
 
-  useEffect(() =>{
-    fetchData();
-  }, []);
+      // Виведення відповіді сервера в консоль
+      console.log('Відповідь сервера:', response.data);
 
-  const onRefresh = () => {
-    setRefreshing(true);
-    fetchData();
+      // Показ повідомлення про успішну відправку SMS
+      Alert.alert('Успішно!', 'SMS успішно відправлено на номер ' + phone);
+    } catch (error) {
+      // Обробка помилок
+      if (error.response) {
+        console.log('Статус:', error.response.status);
+        console.log('Дані:', error.response.data);
+      } else if (error.request) {
+        console.log('Помилка запиту:', error.request);
+      } else {
+        console.log('Помилка:', error.message);
+      }
+
+      // Показ повідомлення про помилку відправки SMS
+      Alert.alert('Помилка!', 'Не вдалося відправити SMS на номер ' + phone);
+    }
   };
 
   return (
-    <View style={styles.container}>
-      <FlatList
-        data={items}
-        renderItem={({ item }) => (
-          <View key={item.id}>
-            <Text>{item.title}</Text>
-            <Text>{item.body}</Text>
-            <Text></Text>
-          </View>
-        )}
-        keyExtractor={(item) => item.id.toString()}
-        refreshControl={
-          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
-        }
-      />
-    </View>
+    <View style={{ padding: 20 }}>
+    <StatusBar/>
+    <TextInput
+      style={{ height: 40, borderColor: 'gray', borderWidth: 1, marginBottom: 10 }}
+      placeholder="Введіть номер телефону"
+      value={phone}
+      onChangeText={setPhone}
+    />
+    <Button title="Відправити SMS" onPress={handleSendSms} />
+  </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
