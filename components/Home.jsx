02831bf5b3d6ef8be0b5557f939e69c0a1a6ac6f
AndriiSmolinskyi <awful.braid@gmail.com>
2023-08-04 12:16:30 +0300

    test-5-04-08

--- components/Home.jsx
+++ components/Home.jsx
@@ -1,54 +1,18 @@
-// // Home.js
-// import React from 'react';
-// import { View, Text, Button } from 'react-native';
-
-// export const Home = ({ navigation }) => {
-//   return (
-//     <View>
-//       <Text>Welcome to Home Page!</Text>
-//       <Button title="Start" onPress={() => navigation.navigate('Start')}></Button>
-//     </View>
-//   );
-// };
-
-// export default Home;
-
-
-
-// components/Home.js
-// import React, { useContext } from 'react';
-// import { View, Text, Button } from 'react-native';
-// import { UserContext } from './UserContext';
-
-// export const Home = ({ navigation }) => {
-//   const { user, setUser } = useContext(UserContext);
-
-//   const handleLogout = () => {
-//     setUser(null);
-//     navigation.navigate('Start');
-//   };
-
-//   return (
-//     <View>
-//       <Text>Welcome to Home Page!</Text>
-//       <Text>Phone: {user.phone}</Text>
-//       <Button title="Logout" onPress={handleLogout} />
-//     </View>
-//   );
-// };
-
-// export default Home;
-
-
 import React, { useContext } from 'react';
 import { View, Text, Button } from 'react-native';
 import { UserContext } from './UserContext';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const Home = ({ navigation }) => {
   const { user, setUser } = useContext(UserContext);
 
-  const handleLogout = () => {
+  const handleLogout = async () => {
     setUser(null);
+    try {
+      await AsyncStorage.removeItem('user'); // Видалити дані з AsyncStorage
+    } catch (error) {
+      console.error('Error removing user data from AsyncStorage:', error);
+    }
     navigation.navigate('Start');
   };
 
--- components/Start.jsx
+++ components/Start.jsx
@@ -1,34 +1,44 @@
-// // Start.js
-// import React from 'react';
-// import { View, Button, StyleSheet } from 'react-native';
-
-// const Start = ({ navigation }) => {
-
-//   return (
-//     <View style={styles.container}>
-//       <Button title="Home" onPress={() => navigation.navigate('Home')}></Button>
-//       <Button title="RegisterPhone" onPress={() => navigation.navigate('RegisterPhone')}></Button>
-//     </View>
-//   );
-// };
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//   },
-// });
-
-// export default Start;
-
-
-
-// components/Start.js
-import React from 'react';
+import React, { useEffect, useContext } from 'react';
 import { View, Button, StyleSheet } from 'react-native';
+import axios from 'axios';
+import { ServerApi } from '../ServerApi';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { UserContext } from './UserContext';
 
 const Start = ({ navigation }) => {
+  const { setUser } = useContext(UserContext);
+
+  useEffect(() => {
+    checkUserAuthentication();
+  }, []);
+
+  const checkUserAuthentication = async () => {
+    try {
+      const userStr = await AsyncStorage.getItem('user');
+      if (userStr) {
+        const user = JSON.parse(userStr);
+        console.log('User data from AsyncStorage:', user);
+
+        const requestData = {
+          login: user.phone.replace('380', ''),
+          password: user.hashedPassword, // Вже захешований пароль
+          WebOrdersApiClientAppToken: 'App_Token',
+        };
+
+        const response = await axios.post(`${ServerApi}/account`, requestData);
+
+        if (response.status === 200) {
+          setUser(user); // Записати дані в UserContext
+          navigation.navigate('Home');
+        } else {
+          console.error('Authentication failed:', response.data);
+        }
+      }
+    } catch (error) {
+      console.error('Error checking user authentication:', error);
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Button title="Home" onPress={() => navigation.navigate('Home')} />
