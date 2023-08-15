d84a201fe3f13d28688b783942a5bfc5f22d11d2
AndriiSmolinskyi <awful.braid@gmail.com>
2023-08-15 13:32:20 +0300

    1

--- components/Home/VisicomSearchWithSuggestions.jsx
+++ components/Home/VisicomSearchWithSuggestions.jsx
@@ -4,30 +4,42 @@ import axios from 'axios';
 
 export const VisicomSearchWithSuggestions = () => {
   const [searchQuery, setSearchQuery] = useState('');
-  const [suggestions, setSuggestions] = useState([]);
+  const [suggestion, setSuggestion] = useState(null); // Змінено state на об'єкт
+  const [timer, setTimer] = useState(null);
 
   const handleSearchChange = async (text) => {
     setSearchQuery(text);
 
-    const apiUrl = 'https://api.visicom.ua/data-api/5.0/uk/geocode.json';
-    const apiKey = '20a396a7d85377ccd96d9491b7889643'; // Замініть на ваш ключ доступу
-
-    try {
-      const response = await axios.get(apiUrl, {
-        params: {
-          lang: 'uk',
-          text: text,
-          limit: 3, // Обмеження на кількість підказок
-          key: apiKey,
-        },
-      });
-
-      // Отримання підказок з API та оновлення стану
-      setSuggestions(response.data);
-    } catch (error) {
-      console.error('Error fetching suggestions:', error);
-      setSuggestions([]);
+    if (timer) {
+      clearTimeout(timer);
     }
+
+    const newTimer = setTimeout(async () => {
+      const apiUrl = 'https://api.visicom.ua/data-api/5.0/uk/geocode.json';
+      const apiKey = '20a396a7d85377ccd96d9491b7889643';
+
+      try {
+        const response = await axios.get(apiUrl, {
+          params: {
+            lang: 'uk',
+            text: text,
+            limit: 1,
+            key: apiKey,
+          },
+        });
+
+        // Отримання підказки з API та оновлення стану
+        const suggestionData = response.data.properties; // Один об'єкт
+        setSuggestion(suggestionData);
+        console.log(suggestionData);
+      } catch (error) {
+        console.error('Error fetching suggestions:', error);
+        setSuggestion(null);
+      }
+    }, 1500); // Затримка 0.5 секунди (500 мілісекунд)
+
+    // Збереження таймера
+    setTimer(newTimer);
   };
 
   return (
@@ -37,6 +49,12 @@ export const VisicomSearchWithSuggestions = () => {
         value={searchQuery}
         onChangeText={handleSearchChange}
       />
+      {suggestion && (
+        <View>
+          <Text>Назва: {suggestion.name}</Text>
+          <Text>Адреса: {suggestion.address}</Text>
+        </View>
+      )}
     </View>
   );
 };
